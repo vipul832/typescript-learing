3e404147cac11fc6e1cf9f49a98e6ca82bdfd2e4
vipulc8 <vipul.c@simformsolutions.com>
2023-03-15 13:47:41 +0530

    Installation and setup Guide

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+//Installation setup idea
+
+/*
+    Step 1) Install ts using Command 
+    
+    "npm install -g typescript".
+
+    Step 2) To Set tsconfig file use command with roodDirectory and outputDirectory 
+    
+    "npm tsc --init --rootdir src --outdir lib".
+
+    Step 3) Create two folder of name src and lib. Inside src folder create a file of any name with extension .ts write the code and save it
+
+    Step 4) Now to covert Typescript to javascript convert use command
+
+    "npm tsc --watch"  // to watch tsc file and to update in js file
+
+
+*/
