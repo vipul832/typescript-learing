57b630b4747f6b04dcfbbbc162c5fba32b34242b
vipulc8 <vipul.c@simformsolutions.com>
2023-03-15 18:51:12 +0530

    Typescritp types and Type aliases

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,57 @@
     Step 4) Now to covert Typescript to javascript convert use command
 
     "npm tsc --watch"  // to watch tsc file and to update in js file
-
-
 */
+
+const num: number = 12; // setting type of variable like number.
+const Name: string = "vishal";
+const age: number = 15;
+let text; // by default it set to be any so, it will accept any value.
+
+text = 1;
+console.log("type of text:", typeof text);
+
+text = "sdfs";
+console.log("type of text:", typeof text);
+
+console.log(num, Name, age, text);
+console.log("type of text");
+
+// object
+
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "amit",
+  age: 12,
+};
+
+console.log("person name", person.name);
+
+//tuple
+
+let role: [number, string];
+
+role = [12, "stag"];
+
+// enum
+
+enum user {
+  ADMIN,
+  DEVELOPER,
+  CLIENT,
+  ENDUSER,
+}
+
+console.log(user.ADMIN);
+
+//Aliases
+
+type combine = number | string; // you can combine any two type and give and aliases name later on you can use as type in other variable
+
+let num1: combine; // num1 can take both number or string as a input;
+
+num1 = 12;
+
+num1 = "sdfa";
