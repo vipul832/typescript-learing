db988a271471cebfdfb9e1845721af36c21977e7
vipulc8 <vipul.c@simformsolutions.com>
2023-03-16 19:16:33 +0530

    inteface and type assertion

--- src/index.ts
+++ src/index.ts
@@ -68,3 +68,53 @@ let num1: combine; // num1 can take both number or string as a input;
 num1 = 12;
 
 num1 = "sdfa";
+
+// interface
+
+interface User {
+  name: string;
+  email: string;
+}
+
+class userInfo {
+  name: string;
+  email: string;
+  constructor(name: string, email: string) {
+    this.name = name;
+    this.email = email;
+  }
+}
+
+const user1: User = new userInfo("vishal", "vishal@gmail.com");
+
+console.log(user1.email);
+
+// function signature
+
+//example 1
+let greeting: (a: string, b: string) => void; //in bracket you defining number of argument that function will take,there data type
+// and function return type. here is void means nothing
+greeting = (name: string, message: string): void => {
+  console.log(`${message},${name}`);
+};
+
+greeting("ashish", "Halo");
+
+//example 2
+let car: (name: string, year: number) => string; // here i specified return type should be string
+
+car = (name: string, year: number): string => {
+  return `Your name is ${name} and model year is ${year}`;
+};
+
+console.log(car("BMW", 2023));
+
+//Querying the Document
+
+const atag = document.querySelector("a")!; //! it means that you saying it never will be null or undefined
+
+console.log(atag);
+
+const input1 = document.querySelector("#form1") as HTMLInputElement;
+
+console.log(input1.value);
